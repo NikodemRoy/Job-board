7e6908472eff1a3ff6852db83f5081369aa20075
NikodemRoy <nikodem.roy@gmail.com>
2022-12-05 23:42:21 +0100

    index prepared for further work

--- APPS/job_board/views.py
+++ APPS/job_board/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def test_view(request):
-    return render(request, "job_board/test.html")
+    return render(request, "job_board/main_index.html")
