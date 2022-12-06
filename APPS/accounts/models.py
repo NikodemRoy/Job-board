7efd06b4cd14228703ee0bfc6790aeb2b1526f54
NikodemRoy <nikodem.roy@gmail.com>
2022-12-06 23:07:51 +0100

    created CustomUser and CustomManager + initial migration

--- APPS/accounts/models.py
+++ APPS/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, blank=False, error_messages={'unique': "A user with that email already exists."})
+
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+
+    def __str__(self):
+        return self.email
--- main_job/settings.py
+++ main_job/settings.py
@@ -130,3 +130,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# adding custom user
+AUTH_USER_MODEL = 'accounts.CustomUser'
