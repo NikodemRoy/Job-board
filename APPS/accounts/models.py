01201d626021efc9ad5345e340f2dfabf74c2f78
NikodemRoy <nikodem.roy@gmail.com>
2022-12-07 13:12:09 +0100

    added User admin + custom forms II

--- APPS/accounts/models.py
+++ APPS/accounts/models.py
@@ -7,12 +7,10 @@ class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(unique=True, blank=False, error_messages={'unique': "A user with that email already exists!"})
 
-
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
-
 
     def __str__(self):
         return self.email
