a510055ae1564b3c720e3e3e0b7a925441a4563d
NikodemRoy <nikodem.roy@gmail.com>
2022-12-07 12:21:58 +0100

    integrated login template

--- APPS/accounts/views.py
+++ APPS/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    return render(request, 'accounts/login.html')
 # Create your views here.
