36f7698aade44514507d05f53726e62d9902b5ea
NikodemRoy <nikodem.roy@gmail.com>
2022-12-08 21:50:52 +0100

    added account role (worker/employer)

--- APPS/accounts/admin.py
+++ APPS/accounts/admin.py
@@ -7,16 +7,16 @@ class CustomUserAdmin(UserAdmin):
     add_form = RegisterForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ('email', 'is_staff', 'is_active',)
-    list_filter = ('email', 'is_staff', 'is_active',)
+    list_display = ('email', 'is_staff', 'is_active', 'account_type',)
+    list_filter = ('email', 'is_staff', 'is_active', 'account_type',)
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'password', 'account_type')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'account_type')}
         ),
     )
     search_fields = ('email',)
--- APPS/accounts/models.py
+++ APPS/accounts/models.py
@@ -2,11 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .managers import CustomUserManager
 
+ACCOUNT_TYPE = [
+    ('worker', 'Worker'),
+    ('employer', "Employer")
+]
 
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(unique=True, blank=False, error_messages={'unique': "A user with that email already exists!"})
 
+    account_type = models.CharField(choices=ACCOUNT_TYPE,  max_length=10)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
