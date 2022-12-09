eefdd0279dfad0c27386d06b1144bb32846e281a
NikodemRoy <nikodem.roy@gmail.com>
2022-12-10 00:40:15 +0100

    working on form in template

--- APPS/accounts/urls.py
+++ APPS/accounts/urls.py
@@ -8,8 +8,6 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_page, name='login'),
-    path('employer-registration/', views.employer_registration, name='employer_registration'),
-    path('worker-registration/', views.worker_registration, name='employer_registration'),
     path('accounts/', include('allauth.urls')),
     path('worker/signup', views.WorkerSignupView.as_view(), name='worker_signup'),
     path('employer/signup', views.EmployerSignupView.as_view(), name='employer_signup'),
--- APPS/accounts/views.py
+++ APPS/accounts/views.py
@@ -10,59 +10,10 @@ from .models import EmployerProfile
 def login_page(request):
     return render(request, 'accounts/login.html')
 
-
-def employer_registration(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.email = user.email.lower()
-            user.account_type = 'employer'
-            user.save()
-
-            # Create user profile
-            profile = EmployerProfile.objects.create(user=user)
-            profile.save()
-
-            messages.success(request, 'Account created succesfull!')
-            # login(request, user)
-            return redirect('index_page')
-        else:
-            # messages.error(request, "Email or password is incorect!")
-            return redirect('index_page')
-
-    context = {'employer_form':form}
-    return render(request, "job_board/main_index.html", context)
-
-
-def worker_registration(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.email = user.email.lower()
-            user.account_type = 'worker'
-            user.save()
-
-            # Create user profile
-            profile = EmployerProfile.objects.create(user=user)
-            profile.save()
-
-            messages.success(request, 'Account created succesfull!')
-            # login(request, user)
-            return redirect('index_page')
-        else:
-            # messages.error(request, "Email or password is incorect!")
-            return redirect('index_page')
-
-    context = {'employer_form':form}
-    return render(request, "job_board/main_index.html", context)
-
-
 # Employee Signup View
 class EmployerSignupView(SignupView):
 
-    template_name = 'users/signup.html'  # Custom template is mandatory
+    template_name = 'accounts/signup_employer.html'  # Custom template is mandatory
     form_class = EmployerSignupForm
     redirect_field_name = 'next'  # Important to redirect user if has next url
 
@@ -75,7 +26,7 @@ class EmployerSignupView(SignupView):
 # Employee Signup View
 class WorkerSignupView(SignupView):
 
-    template_name = 'users/signup.html'  # Custom template is mandatory
+    template_name = 'accounts/signup_worker.html'  # Custom template is mandatory
     form_class = WorkerSignupForm
     redirect_field_name = 'next'  # Important to redirect user if has next url
 
@@ -84,3 +35,54 @@ class WorkerSignupView(SignupView):
         ret = super(WorkerSignupForm, self).get_context_data(**kwargs)
         ret.update(self.kwargs)
         return ret
+
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+# def employer_registration(request):
+#     if request.method == 'POST':
+#         form = RegisterForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.email = user.email.lower()
+#             user.account_type = 'employer'
+#             user.save()
+
+#             # Create user profile
+#             profile = EmployerProfile.objects.create(user=user)
+#             profile.save()
+
+#             messages.success(request, 'Account created succesfull!')
+#             # login(request, user)
+#             return redirect('index_page')
+#         else:
+#             # messages.error(request, "Email or password is incorect!")
+#             return redirect('index_page')
+
+#     context = {'employer_form':form}
+#     return render(request, "job_board/main_index.html", context)
+
+
+# def worker_registration(request):
+#     if request.method == 'POST':
+#         form = RegisterForm(request.POST)
+#         if form.is_valid():
+#             user = form.save(commit=False)
+#             user.email = user.email.lower()
+#             user.account_type = 'worker'
+#             user.save()
+
+#             # Create user profile
+#             profile = EmployerProfile.objects.create(user=user)
+#             profile.save()
+
+#             messages.success(request, 'Account created succesfull!')
+#             # login(request, user)
+#             return redirect('index_page')
+#         else:
+#             # messages.error(request, "Email or password is incorect!")
+#             return redirect('index_page')
+
+#     context = {'employer_form':form}
+#     return render(request, "job_board/main_index.html", context)
+
