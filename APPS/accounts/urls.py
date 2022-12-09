628718815c579c27df535507c050c2de4a5ce750
NikodemRoy <nikodem.roy@gmail.com>
2022-12-09 23:42:18 +0100

    base allauth app structure

--- APPS/accounts/urls.py
+++ APPS/accounts/urls.py
@@ -1,5 +1,5 @@
 from django import views
-from django.urls import path
+from django.urls import path, include
 
 # from django.conf import settings
 # from django.conf.urls.static import static
@@ -10,6 +10,7 @@ urlpatterns = [
     path('login/', views.login_page, name='login'),
     path('employer-registration/', views.employer_registration, name='employer_registration'),
     path('worker-registration/', views.worker_registration, name='employer_registration'),
+    path('accounts/', include('allauth.urls')),
 ]
 
 
--- main_job/settings.py
+++ main_job/settings.py
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     # developer added APPS
     'APPS.job_board',
     'APPS.accounts',
+    # The following apps are required for allauth
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -65,9 +70,20 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # needed for allaouth
+                'django.template.context_processors.request'
             ],
         },
     },
+]
+
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 WSGI_APPLICATION = 'main_job.wsgi.application'
@@ -134,3 +150,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # adding custom user
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+#allauth
+SITE_ID = 1
