6913f7e91fe6ce6759cde72c5c803fe18a382584
NikodemRoy <nikodem.roy@gmail.com>
2023-01-29 19:15:15 +0100

    new models for worker profile

--- APPS/profiles/models.py
+++ APPS/profiles/models.py
@@ -11,10 +11,78 @@ class EmployerProfile(models.Model):
 
 class WorkerProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, db_index=True)
+
     name = models.CharField(max_length=96, blank=True)
     surname = models.CharField(max_length=96, blank=True)
+    phone_number = models.CharField(max_length=96, blank=True)
+
+
+    languages = models.CharField(max_length=96, blank=True)
+    job_title = models.CharField(max_length=96, blank=True)
+    work_experience = models.CharField(max_length=4, blank=True)
+    birth_year = models.CharField(max_length=4, blank=True)
+
+    country = models.CharField(max_length=96, blank=True)
+    city = models.CharField(max_length=96, blank=True)
+
+    profile_description = models.TextField(max_length=5000, blank=True)
+
+    fb_link = models.CharField(max_length=96, blank=True)
+    twitter_link = models.CharField(max_length=96, blank=True)
+    linkedin_link = models.CharField(max_length=96, blank=True)
+    instagram_link = models.CharField(max_length=96, blank=True)
+    youtube_link = models.CharField(max_length=96, blank=True)
 
     def __str__(self):
         return self.user.email
 
-# Create your models here.
+class ShortSkills(models.Model):
+    profile = models.ForeignKey(WorkerProfile, on_delete=models.CASCADE, null=True, blank=True)
+    skill = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.skill)
+
+class Skills(models.Model):
+    profile = models.ForeignKey(WorkerProfile, on_delete=models.CASCADE, null=True, blank=True)
+    skill = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    work_experience = models.CharField(max_length=4, blank=True)
+
+    def __str__(self):
+        return str(self.skill)
+
+class EmploymentHistory(models.Model):
+    profile = models.ForeignKey(WorkerProfile, on_delete=models.CASCADE, null=True, blank=True)
+    company_name = models.CharField(max_length=200, blank=True, null=True)
+    position_name = models.CharField(max_length=200, blank=True, null=True)
+    job_description = models.CharField(max_length=200, blank=True, null=True)
+
+    from_date = models.CharField(max_length=15, blank=True, null=True)
+    to_date = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.position_name)
+
+class EducationHistory(models.Model):
+    profile = models.ForeignKey(WorkerProfile, on_delete=models.CASCADE, null=True, blank=True)
+    schoole_name = models.CharField(max_length=200, blank=True, null=True)
+    schoole_description = models.CharField(max_length=200, blank=True, null=True)
+
+    from_date = models.CharField(max_length=15, blank=True, null=True)
+    to_date = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.schoole_name)
+
+class CertificatesHistory(models.Model):
+    profile = models.ForeignKey(WorkerProfile, on_delete=models.CASCADE, null=True, blank=True)
+    certificate_name = models.CharField(max_length=200, blank=True, null=True)
+    company_name = models.CharField(max_length=200, blank=True, null=True)
+    job_description = models.CharField(max_length=200, blank=True, null=True)
+
+    from_date = models.CharField(max_length=15, blank=True, null=True)
+    to_date = models.CharField(max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.certificate_name)
